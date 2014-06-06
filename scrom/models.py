a84ea24cb47faf4110b84ecc1e1651772ebfacef
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2014-06-06 16:57:27 -0500

    custom models

--- scrom/models.py
+++ scrom/models.py
@@ -1,3 +1,4 @@
+import json
 from sqlalchemy import (
     Column,
     Index,
@@ -7,7 +8,7 @@ from sqlalchemy import (
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.dialects.postgresql import HSTORE
+from sqlalchemy.types import TypeDecorator, VARCHAR
 
 from sqlalchemy.orm import (
     scoped_session,
@@ -20,16 +21,23 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+class JSONEncodedDict(TypeDecorator):
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+            if value is not None:
+                value = json.dumps(value)
+
+            return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
 
 
 class Project(Base):
     __tablename__ = 'projects'
     uid = Column(Integer, primary_key=True)
-    structure = Column(MutableDict.as_mutable(HSTORE))
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    name = Column(Text)
+    structure = Column(MutableDict.as_mutable(JSONEncodedDict))
